c73e77c67e0679fe869f6472f17c9ce8ff1b97ef
tormodin <torm@kth.se>
2023-11-14 13:34:45 +0100

    code

--- Project 1/forceandmoments2.py
+++ Project 1/forceandmoments2.py
@@ -238,6 +238,9 @@ print("\nDistributed Muscle Forces at Ankle (N):")
 print("Ankle Dorsiflexor Force:", ankle_dorsiflexor_force)
 print("Ankle Plantarflexor Force:", ankle_plantarflexor_force)
 #code to give correct variable names
+ankle_force_y+footRY=foot_mass*ankle_angular_acceleration_y
+ankle_force_z=foot_mass*ankle_angular_acceleration_z-footRX
+ankle_force_y=foot_mass-footRX
 
 #from your notes x to y, y to z, z to x
 # Moment on the Foot
